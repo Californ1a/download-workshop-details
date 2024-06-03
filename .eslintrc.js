736633f9206533d0b2f6c2fc906b8f83a924887d
Californ1a <rk_california@yahoo.com>
2024-06-03 08:54:39 -0400

    re-add fake react version?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 /** @type { import("eslint").Linter.Config } */
 const config = {
 	extends: [
-		'./node_modules\\dts-cli\\conf\\eslint-config-react-app\\index.js',
-		'./node_modules\\eslint-config-prettier\\index.js',
+		'./node_modules/dts-cli/conf/eslint-config-react-app/index.js',
+		'./node_modules/eslint-config-prettier/index.js',
 		'plugin:prettier/recommended',
 	],
+	settings: {
+		react: {
+			version: '999.999.999',
+		},
+	},
 };
 
 module.exports = config;
